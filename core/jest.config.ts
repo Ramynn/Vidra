ae3ed79a0f06253706f016ab2fbfbfc48b708f67
Ramin Mohammadi <lvlohammadi@gmail.com>
2023-11-17 15:59:18 +0330

    feat: setup testing libraries

---
+++ core/jest.config.ts
@@ -0,0 +1,13 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/*.test.(ts|tsx)'],
+  moduleFileExtensions: ['ts', 'json', 'node', 'js', 'tsx'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFilesAfterEnv: ['../jest-setup.ts'],
+  bail: true,
+};
+
+export default config;
