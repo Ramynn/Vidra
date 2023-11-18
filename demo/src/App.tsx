c5529eb2fe90e2c644656ebfed7a7d60427a3781
Ramin Mohammadi <lvlohammadi@gmail.com>
2023-11-18 16:13:48 +0330

    feat: improve stylesheets

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,9 +1,12 @@
 import {ArrowLeftIcon} from 'vidra';
+import 'vidra/styles/defaut.scss';
+import './demo.scss';
 
 export function App() {
   return (
     <>
       <ArrowLeftIcon />
+      Demo App
     </>
   );
 }
